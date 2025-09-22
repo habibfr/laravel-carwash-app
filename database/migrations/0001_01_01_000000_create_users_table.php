2f24ee1e5494ff2b2f1f32b8d3bb4fa169190dab
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2025-09-22 14:59:27 +0700

    Refactor user migration to use UUID for primary key and add additional fields

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,12 +13,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid(column: 'id')->primary()->default(value: DB::raw('gen_random_uuid()'));
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable();
             $table->string('password');
+            $table->string('profile_picture')->nullable();
+            $table->enum('status', ['active', 'inactive', 'suspended'])->default('active');
             $table->rememberToken();
+            $table->softDeletes();
+
+            $table->index(['email', 'status']);
             $table->timestamps();
         });
 
